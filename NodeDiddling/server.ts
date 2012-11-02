140bab8999659157665fec7c5b4e71c183b78c5c
Dave Dixon <dave.dixon@allgress.com>
2012-11-02 09:38:23 -0700

    Move solution folder. Make server tastiness.

---
+++ NodeDiddling/server.ts
@@ -0,0 +1,7 @@
+﻿///<reference path='node.d.ts'/>
+import http = module("http");
+
+http.createServer(function (request, response) {
+    response.writeHead(200, { "Content-type": "text/html" });
+    response.end("Woohoo!");
+}).listen(8080);
