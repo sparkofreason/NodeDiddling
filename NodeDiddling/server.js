340249ade1699645f66b4bc9c9b2d0f1e89e596b
Dave Dixon <dave.dixon@allgress.com>
2012-11-02 14:58:11 -0700

    Cachification

--- NodeDiddling/server.js
+++ NodeDiddling/server.js
@@ -6,12 +6,34 @@ var mimeTypes = {
     ".html": "text/html",
     ".css": "text/css"
 };
+var cache = {
+};
+function cacheAndDeliver(f, cb) {
+    fs.stat(f, function (err, stats) {
+        var lastChanged = stats.mtime;
+        var isUpdated = (cache[f]) && lastChanged > cache[f].timestamp;
+        if(!cache[f] || isUpdated) {
+            fs.readFile(f, function (err, data) {
+                if(!err) {
+                    cache[f] = {
+                        content: data,
+                        timestamp: Date.now()
+                    };
+                }
+                cb(err, data);
+            });
+            return;
+        }
+        console.log("loading " + f + " from cache");
+        cb(null, cache[f].content);
+    });
+}
 http.createServer(function (request, response) {
     var lookup = path.basename(decodeURI(request.url)) || "index.html";
     var f = "content/" + lookup;
     fs.exists(f, function (exists) {
         if(exists) {
-            fs.readFile(f, function (err, data) {
+            cacheAndDeliver(f, function (err, data) {
                 if(err) {
                     response.writeHead(500);
                     response.end("Server error!");
