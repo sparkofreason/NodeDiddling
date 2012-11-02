c8a4b012a706122a896c1824eb40f039a9abc6b5
Dave Dixon <dave.dixon@allgress.com>
2012-11-02 10:01:19 -0700

    Stupid routing

--- NodeDiddling/server.js
+++ NodeDiddling/server.js
@@ -1,8 +1,34 @@
 var http = require("http")
+var path = require("path")
+var pages = [
+    {
+        route: "",
+        output: "Woohoo!"
+    }, 
+    {
+        route: "about",
+        output: "A simple routing with Node example"
+    }, 
+    {
+        route: "another page",
+        output: function () {
+            return "Here's " + this.route;
+        }
+    }
+];
 http.createServer(function (request, response) {
-    response.writeHead(200, {
-        "Content-type": "text/html"
+    var lookup = path.basename(decodeURI(request.url));
+    pages.forEach(function (page) {
+        if(page.route === lookup) {
+            response.writeHead(200, {
+                "Content-type": "text/html"
+            });
+            response.end(typeof page.output === "function" ? page.output() : page.output);
+        }
     });
-    response.end("Woohoo!");
+    if(!response.finished) {
+        response.writeHead(404);
+        response.end("Page not found");
+    }
 }).listen(8080);
 
--- NodeDiddling/server.ts
+++ NodeDiddling/server.ts
@@ -1,7 +1,22 @@
 ﻿///<reference path='node.d.ts'/>
 import http = module("http");
+import path = module("path");
+var pages = [
+    { route: "", output: "Woohoo!" },
+    { route: "about", output: "A simple routing with Node example" },
+    { route: "another page", output: function () { return "Here's " + this.route; } }
+];
 
 http.createServer(function (request, response) {
-    response.writeHead(200, { "Content-type": "text/html" });
-    response.end("Woohoo!");
+    var lookup = path.basename(decodeURI(request.url));
+    pages.forEach(function (page) {
+        if (page.route === lookup) {
+            response.writeHead(200, { "Content-type": "text/html" });
+            response.end(typeof page.output === "function" ? page.output() : page.output);
+        }
+    });
+    if (!response.finished) {
+        response.writeHead(404);
+        response.end("Page not found");
+    }
 }).listen(8080);
