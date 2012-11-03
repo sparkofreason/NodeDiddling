aa1901c87217c20d19bee861530f08cc0c7bd73d
Dave Dixon <dave.dixon@allgress.com>
2012-11-03 06:18:18 -0700

    Use arguments for fetch

--- NodeDiddling/Chapter2/fetch.ts
+++ NodeDiddling/Chapter2/fetch.ts
@@ -1,8 +1,17 @@
 ﻿///<reference path='../node.d.ts'/>
 import http = module("http");
+import url = module("url");
 var urlOpts = { host: "www.nodejs.org", path:"/", port: 80};
+if (process.argv[2]) {
+    if (!process.argv[2].match("http://")) {
+        process.argv[2] = "http://" + process.argv[2];
+    }
+    urlOpts = url.parse(process.argv[2]);
+}
 http.get(urlOpts, function (response) {
     response.on("data", function (chunk) {
         console.log(chunk.toString());
     });
+}).on("error", function(e){
+    console.log("Error: ", e.message)
 });
