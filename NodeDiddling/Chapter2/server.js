97904c3324d6a786a727c7eee2b2ad922900416a
Dave Dixon <dave.dixon@allgress.com>
2012-11-03 05:43:28 -0700

    Use connect middleware

--- NodeDiddling/Chapter2/server.js
+++ NodeDiddling/Chapter2/server.js
@@ -1,10 +1,8 @@
-var http = require("http")
+var connect = require("connect");
 var fs = require("fs")
-var querystring = require("querystring")
 var util = require("util")
-var maxData = 2 * 1024 * 1024;
 var form = fs.readFileSync("form.html");
-http.createServer(function (request, response) {
+connect(connect.limit("64kb"), connect.bodyParser(), function (request, response) {
     if(request.method === "GET") {
         response.writeHead(200, {
             "Content-type": "text/html"
@@ -12,25 +10,8 @@ http.createServer(function (request, response) {
         response.end(form);
     } else {
         if(request.method === "POST") {
-            var postData = "";
-            request.on("data", function (chunk) {
-                postData += chunk;
-                if(postData.length > maxData) {
-                    postData = "";
-                    this.pause();
-                    response.writeHead(403);
-                    response.end("Too large");
-                }
-            });
-            request.on("end", function () {
-                if(!postData) {
-                    response.end();
-                    return;
-                }
-                var postDataObject = querystring.parse(postData);
-                console.log("User Posted:\n", postData);
-                response.end("You posted:\n" + util.inspect(postDataObject));
-            });
+            console.log("User Posted:\n", request.body);
+            response.end("You posted:\n" + util.inspect(request.body));
         }
     }
 }).listen(8080);
--- NodeDiddling/Chapter2/server.ts
+++ NodeDiddling/Chapter2/server.ts
@@ -1,35 +1,16 @@
 ﻿///<reference path='../node.d.ts'/>
-import http = module("http");
+var connect = require("connect");
 import fs = module("fs");
-import querystring = module("querystring");
 import util = module("util");
 
-var maxData = 2 * 1024 * 1024;
-
 var form = fs.readFileSync("form.html");
-http.createServer(function (request, response) {
-    if (request.method === "GET") {
-        response.writeHead(200, { "Content-type": "text/html" });
-        response.end(form);
-    } else if (request.method === "POST") {
-        var postData = "";
-        request.on("data", function (chunk) {
-            postData += chunk;
-            if (postData.length > maxData) {
-                postData = "";
-                this.pause();
-                response.writeHead(403);
-                response.end("Too large");
-            }
-        });
-        request.on("end", function () {
-            if (!postData) {
-                response.end();
-                return;
-            }
-            var postDataObject = querystring.parse(postData);
-            console.log("User Posted:\n", postData);
-            response.end("You posted:\n" + util.inspect(postDataObject));
-        });
-    }
-}).listen(8080);
+connect(connect.limit("64kb"), connect.bodyParser(),
+    function (request, response) {
+        if (request.method === "GET") {
+            response.writeHead(200, { "Content-type": "text/html" });
+            response.end(form);
+        } else if (request.method === "POST") {
+            console.log("User Posted:\n", request.body);
+            response.end("You posted:\n" + util.inspect(request.body));
+        }
+    }).listen(8080);
